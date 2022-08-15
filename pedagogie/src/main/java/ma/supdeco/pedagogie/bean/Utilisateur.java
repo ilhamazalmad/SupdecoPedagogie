9032e0d75239d956b135480a8be6b893ed1e2105
ilhamazalmad <ilhamazalmad11@gmail.com>
2022-08-15 18:18:27 +0100

    Création des relations des associations entre les tables - 2

--- pedagogie/src/main/java/ma/supdeco/pedagogie/bean/Utilisateur.java
+++ pedagogie/src/main/java/ma/supdeco/pedagogie/bean/Utilisateur.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -22,116 +23,12 @@ public class Utilisateur implements Serializable {
 	private String motDePasse;
 	private boolean active;
 
-	public java.util.Collection<RoleUtilisateur> roleUtilisateur;
+	@ManyToMany
+	private List<Role> roles;
 
 	public Utilisateur() {
 		super();
 	}
 
-	public Utilisateur(int idUtilisateur, String nom, String prenom, String motDePasse, boolean active,
-			Collection<RoleUtilisateur> roleUtilisateur) {
-		super();
-		this.idUtilisateur = idUtilisateur;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.motDePasse = motDePasse;
-		this.active = active;
-		this.roleUtilisateur = roleUtilisateur;
-	}
-
-	/** @pdGenerated default getter */
-	public java.util.Collection<RoleUtilisateur> getRoleUtilisateur() {
-		if (roleUtilisateur == null)
-			roleUtilisateur = new java.util.HashSet<RoleUtilisateur>();
-		return roleUtilisateur;
-	}
-
-	/** @pdGenerated default iterator getter */
-	public java.util.Iterator getIteratorRoleUtilisateur() {
-		if (roleUtilisateur == null)
-			roleUtilisateur = new java.util.HashSet<RoleUtilisateur>();
-		return roleUtilisateur.iterator();
-	}
-
-	/**
-	 * @pdGenerated default setter
-	 * @param newRoleUtilisateur
-	 */
-	public void setRoleUtilisateur(java.util.Collection<RoleUtilisateur> newRoleUtilisateur) {
-		removeAllRoleUtilisateur();
-		for (java.util.Iterator iter = newRoleUtilisateur.iterator(); iter.hasNext();)
-			addRoleUtilisateur((RoleUtilisateur) iter.next());
-	}
-
-	/**
-	 * @pdGenerated default add
-	 * @param newRoleUtilisateur
-	 */
-	public void addRoleUtilisateur(RoleUtilisateur newRoleUtilisateur) {
-		if (newRoleUtilisateur == null)
-			return;
-		if (this.roleUtilisateur == null)
-			this.roleUtilisateur = new java.util.HashSet<RoleUtilisateur>();
-		if (!this.roleUtilisateur.contains(newRoleUtilisateur))
-			this.roleUtilisateur.add(newRoleUtilisateur);
-	}
-
-	/**
-	 * @pdGenerated default remove
-	 * @param oldRoleUtilisateur
-	 */
-	public void removeRoleUtilisateur(RoleUtilisateur oldRoleUtilisateur) {
-		if (oldRoleUtilisateur == null)
-			return;
-		if (this.roleUtilisateur != null)
-			if (this.roleUtilisateur.contains(oldRoleUtilisateur))
-				this.roleUtilisateur.remove(oldRoleUtilisateur);
-	}
-
-	/** @pdGenerated default removeAll */
-	public void removeAllRoleUtilisateur() {
-		if (roleUtilisateur != null)
-			roleUtilisateur.clear();
-	}
-
-	public int getIdUtilisateur() {
-		return idUtilisateur;
-	}
-
-	public void setIdUtilisateur(int idUtilisateur) {
-		this.idUtilisateur = idUtilisateur;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getMotDePasse() {
-		return motDePasse;
-	}
-
-	public void setMotDePasse(String motDePasse) {
-		this.motDePasse = motDePasse;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
 
 }
--- pedagogie/src/main/java/ma/supdeco/pedagogie/bean/Ville.java
+++ pedagogie/src/main/java/ma/supdeco/pedagogie/bean/Ville.java
@@ -2,12 +2,19 @@ package ma.supdeco.pedagogie.bean;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
 
 @Entity
 @Table(name = "ville")
@@ -18,72 +25,23 @@ public class Ville implements Serializable {
 	private int idVille;
 	private String ville;
 
-	public java.util.Collection<Etudiant> etudiant;
+	@OneToMany(mappedBy = "ville", cascade = CascadeType.MERGE)
+	private List<Etudiant> etudiants;
+
+	@ManyToOne
+	@JoinColumn(name = "idPays", nullable = false)
+	private Pays pays;
 
 	public Ville() {
 		super();
 	}
 
-	public Ville(int idVille, String ville, Collection<Etudiant> etudiant) {
+	public Ville(int idVille, String ville, List<Etudiant> etudiants, Pays pays) {
 		super();
 		this.idVille = idVille;
 		this.ville = ville;
-		this.etudiant = etudiant;
-	}
-
-	/** @pdGenerated default getter */
-	public java.util.Collection<Etudiant> getEtudiant() {
-		if (etudiant == null)
-			etudiant = new java.util.HashSet<Etudiant>();
-		return etudiant;
-	}
-
-	/** @pdGenerated default iterator getter */
-	public java.util.Iterator getIteratorEtudiant() {
-		if (etudiant == null)
-			etudiant = new java.util.HashSet<Etudiant>();
-		return etudiant.iterator();
-	}
-
-	/**
-	 * @pdGenerated default setter
-	 * @param newEtudiant
-	 */
-	public void setEtudiant(java.util.Collection<Etudiant> newEtudiant) {
-		removeAllEtudiant();
-		for (java.util.Iterator iter = newEtudiant.iterator(); iter.hasNext();)
-			addEtudiant((Etudiant) iter.next());
-	}
-
-	/**
-	 * @pdGenerated default add
-	 * @param newEtudiant
-	 */
-	public void addEtudiant(Etudiant newEtudiant) {
-		if (newEtudiant == null)
-			return;
-		if (this.etudiant == null)
-			this.etudiant = new java.util.HashSet<Etudiant>();
-		if (!this.etudiant.contains(newEtudiant))
-			this.etudiant.add(newEtudiant);
-	}
-
-	/**
-	 * @pdGenerated default remove
-	 * @param oldEtudiant
-	 */
-	public void removeEtudiant(Etudiant oldEtudiant) {
-		if (oldEtudiant == null)
-			return;
-		if (this.etudiant != null)
-			if (this.etudiant.contains(oldEtudiant))
-				this.etudiant.remove(oldEtudiant);
-	}
-
-	/** @pdGenerated default removeAll */
-	public void removeAllEtudiant() {
-		if (etudiant != null)
-			etudiant.clear();
+		this.etudiants = etudiants;
+		this.pays = pays;
 	}
 
 	public int getIdVille() {
@@ -100,6 +58,22 @@ public class Ville implements Serializable {
 
 	public void setVille(String ville) {
 		this.ville = ville;
+	}
+
+	public List<Etudiant> getEtudiants() {
+		return etudiants;
+	}
+
+	public void setEtudiants(List<Etudiant> etudiants) {
+		this.etudiants = etudiants;
+	}
+
+	public Pays getPays() {
+		return pays;
+	}
+
+	public void setPays(Pays pays) {
+		this.pays = pays;
 	}
 
 }
